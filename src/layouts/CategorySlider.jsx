17764a8306c26eb904714f8330cb530fc38fceda
Jawad Hossain Mahi <jawadhossainmahi@gmail.com>
2025-04-14 14:00:03 +0600

    website static to react convertion

---
+++ src/layouts/CategorySlider.jsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import Icon_vegetable_broccoli from "../assets/images/icon-vegetables-broccoli.png"
+import Icon_bread_bague from "../assets/images/icon-bread-baguette.png"
+import Icon_soft_drink from "../assets/images/icon-soft-drinks-bottle.png"
+import Icon_wine_g from "../assets/images/icon-wine-glass-bottle.png"
+import Icon_animal_p from "../assets/images/icon-animal-products-drumsticks.png"
+import Icon_bread_h from "../assets/images/icon-bread-herb-flour.png";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import { Navigation } from 'swiper/modules'
+function CategorySlider() {
+    return (
+        <>
+            <section className="py-5 overflow-hidden">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+
+                            <div className="section-header d-flex flex-wrap justify-content-between mb-5">
+                                <h2 className="section-title">Category</h2>
+
+                                <div className="d-flex align-items-center">
+                                    <a href="#" className="btn-link text-decoration-none">View All Categories →</a>
+                                    <div className="swiper-buttons">
+                                        <button className="swiper-prev category-carousel-prev btn btn-yellow">❮</button>
+                                        <button className="swiper-next category-carousel-next btn btn-yellow">❯</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Swiper navigation={{
+                                nextEl: '.category-carousel-next',
+                                prevEl: '.category-carousel-prev',
+                            }} slidesPerView={6} spaceBetween={30} className="category-carousel" modules={[Navigation]}>
+                                <SwiperSlide><a href="index.html" className="nav-link category-item swiper-slide">
+                                    <img src={Icon_vegetable_broccoli} alt="Category Thumbnail" />
+                                    <h3 className="category-title">Fruits & Veges</h3>
+                                </a></SwiperSlide>
+                                <SwiperSlide>   <a href="index.html" className="nav-link category-item swiper-slide">
+                                    <img src={Icon_bread_bague} alt="Category Thumbnail" />
+                                    <h3 className="category-title">Breads & Sweets</h3>
+                                </a></SwiperSlide>
+                                <SwiperSlide>
+                                    <a href="index.html" className="nav-link category-item swiper-slide">
+                                        <img src={Icon_soft_drink} alt="Category Thumbnail" />
+                                        <h3 className="category-title">Fruits & Veges</h3>
+                                    </a></SwiperSlide>
+                                <SwiperSlide>  <a href="index.html" className="nav-link category-item swiper-slide">
+                                    <img src={Icon_wine_g} alt="Category Thumbnail" />
+                                    <h3 className="category-title">Fruits & Veges</h3>
+                                </a></SwiperSlide>
+                                <SwiperSlide> <a href="index.html" className="nav-link category-item swiper-slide">
+                                    <img src={Icon_animal_p} alt="Category Thumbnail" />
+                                    <h3 className="category-title">Fruits & Veges</h3>
+                                </a></SwiperSlide>
+                                <SwiperSlide>
+                                    <a href="index.html" className="nav-link category-item swiper-slide">
+                                        <img src={Icon_bread_h} alt="Category Thumbnail" />
+                                        <h3 className="category-title">Fruits & Veges</h3>
+                                    </a></SwiperSlide>
+                                <SwiperSlide>
+
+                                    <a href="index.html" className="nav-link category-item swiper-slide">
+                                        <img src={Icon_vegetable_broccoli} alt="Category Thumbnail" />
+                                        <h3 className="category-title">Fruits & Veges</h3>
+                                    </a></SwiperSlide>
+                                <SwiperSlide> <a href="index.html" className="nav-link category-item swiper-slide">
+                                    <img src={Icon_vegetable_broccoli} alt="Category Thumbnail" />
+                                    <h3 className="category-title">Fruits & Veges</h3>
+                                </a></SwiperSlide>
+                                <SwiperSlide>
+                                    <a href="index.html" className="nav-link category-item swiper-slide">
+                                        <img src={Icon_vegetable_broccoli} alt="Category Thumbnail" />
+                                        <h3 className="category-title">Fruits & Veges</h3>
+                                    </a></SwiperSlide>
+                                <SwiperSlide>
+                                    <a href="index.html" className="nav-link category-item swiper-slide">
+                                        <img src={Icon_vegetable_broccoli} alt="Category Thumbnail" />
+                                        <h3 className="category-title">Fruits & Veges</h3>
+                                    </a></SwiperSlide>
+                                <SwiperSlide>
+                                    <a href="index.html" className="nav-link category-item swiper-slide">
+                                        <img src={Icon_vegetable_broccoli} alt="Category Thumbnail" />
+                                        <h3 className="category-title">Fruits & Veges</h3>
+                                    </a>
+                                </SwiperSlide>
+                                <SwiperSlide>
+                                    <a href="index.html" className="nav-link category-item swiper-slide">
+                                        <img src={Icon_vegetable_broccoli} alt="Category Thumbnail" />
+                                        <h3 className="category-title">Fruits & Veges</h3>
+                                    </a>
+                                </SwiperSlide>
+                            </Swiper>
+                            {/* <div className=" swiper">
+                                <div className="swiper-wrapper">
+                                    
+                                 
+                               
+                                  
+                                   
+                                   
+
+                                </div>
+                            </div> */}
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default CategorySlider
---
+++ src/layouts/HomeBanner.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import BgPattern from "../assets/images/background-pattern.jpg";
+import Pthumb_1 from "../assets/images/product-thumb-1.png";
+import Pthumb_2 from "../assets/images/product-thumb-2.png";
+import Ad_img_1 from "../assets/images/ad-image-1.png";
+import Ad_img_2 from "../assets/images/ad-image-2.png";
+function HomeBanner() {
+    return (
+        <>
+            <section className="py-3"
+                style={{ backgroundImage: `url(${BgPattern})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
+            >
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+
+                            <div className="banner-blocks">
+
+                                <div className="banner-ad large bg-info block-1">
+
+                                    <Swiper pagination={true} modules={[Pagination]} className='main-swiper'>
+                                        <SwiperSlide>
+                                            <div className="row banner-content p-5">
+                                                <div className="content-wrapper col-md-7">
+                                                    <div className="categories my-3">100% natural</div>
+                                                    <h3 className="display-4">Fresh Smoothie & Summer Juice</h3>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim massa diam elementum.</p>
+                                                    <a href="#" className="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1 px-4 py-3 mt-3">Shop Now</a>
+                                                </div>
+                                                <div className="img-wrapper col-md-5">
+                                                    <img src="" className="img-fluid" />
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
+
+                                        <SwiperSlide>
+                                            <div className="row banner-content p-5">
+                                                <div className="content-wrapper col-md-7">
+                                                    <div className="categories mb-3 pb-3">100% natural</div>
+                                                    <h3 className="banner-title">Fresh Smoothie & Summer Juice</h3>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim massa diam elementum.</p>
+                                                    <a href="#" className="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">Shop Collection</a>
+                                                </div>
+                                                <div className="img-wrapper col-md-5">
+                                                    <img src={Pthumb_1} className="img-fluid" />
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
+
+                                        <SwiperSlide>
+                                            <div className="row banner-content p-5">
+                                                <div className="content-wrapper col-md-7">
+                                                    <div className="categories mb-3 pb-3">100% natural</div>
+                                                    <h3 className="banner-title">Heinz Tomato Ketchup</h3>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim massa diam elementum.</p>
+                                                    <a href="#" className="btn btn-outline-dark btn-lg text-uppercase fs-6 rounded-1">Shop Collection</a>
+                                                </div>
+                                                <div className="img-wrapper col-md-5">
+                                                    <img src={Pthumb_2} className="img-fluid" />
+                                                </div>
+                                            </div>
+                                        </SwiperSlide>
+                                    </Swiper>
+                                    {/* <div className="swiper main-swiper">
+                                        <div className="swiper-wrapper">
+
+
+                                        </div>
+
+                                        <div className="swiper-pagination"></div>
+
+                                    </div> */}
+                                </div>
+
+                                <div className="banner-ad bg-success-subtle block-2"
+                                    style={{ background: `url(${Ad_img_1})`, backgroundRepeat: "no-repeat", backgroundPosition: "right bottom" }}
+                                >
+                                    <div className="row banner-content p-5">
+
+                                        <div className="content-wrapper col-md-7">
+                                            <div className="categories sale mb-3 pb-3">20% off</div>
+                                            <h3 className="banner-title">Fruits & Vegetables</h3>
+                                            <a href="#" className="d-flex align-items-center nav-link">Shop Collection <svg width="24" height="24"><use xlinkHref="#arrow-right"></use></svg></a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div className="banner-ad bg-danger block-3"
+                                    style={{ background: `url(${Ad_img_2})`, backgroundPosition: "right bottom", backgroundRepeat: "no-repeat" }} >
+                                    <div className="row banner-content p-5">
+
+                                        <div className="content-wrapper col-md-7">
+                                            <div className="categories sale mb-3 pb-3">15% off</div>
+                                            <h3 className="item-title">Baked Products</h3>
+                                            <a href="#" className="d-flex align-items-center nav-link">Shop Collection <svg width="24" height="24"><use xlinkHref="#arrow-right"></use></svg></a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                            {/* <!-- / Banner Blocks --> */}
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default HomeBanner   
